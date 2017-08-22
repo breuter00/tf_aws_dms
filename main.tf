4dd7ec0f57b3cb06800306b7ea8299ebb1172c9e
breuter00 <29645593+breuter00@users.noreply.github.com>
2017-08-22 17:14:26 -0500

    comments on main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#create a dms instance from module
 module "dms_instance" {
     source = "./modules/dms_instance"
     dms_instance_id = "dms-repl-n-staging"
@@ -5,12 +6,14 @@ module "dms_instance" {
     #dms_instance_multi_az
 
 }
+
+#create a task to replicate source schema
 module "dms_task_tick" {
     source = "./modules/dms_task"
+    source_schema           = "tick"
     dms_instance_arn        = "${module.dms_instance.dms_instance_arn}"
     dms_instance_name       = "${module.dms_instance.dms_instance_name}"
     dms_instance_key_arn    = "${module.dms_instance.dms_key_arn}"
-    source_schema           = "tick"
     source_url              = "jdbc://somesource"
     source_username         = "src_user"
     source_password         = "src_pass"
@@ -18,12 +21,14 @@ module "dms_task_tick" {
     target_username         = "tgt_user"
     target_password         = "tgt_pass"
 }
+
+#create a second task to replicate, another source schema
 module "dms_task_tock" {
     source = "./modules/dms_task"
+    source_schema           = "tock"
     dms_instance_arn        = "${module.dms_instance.dms_instance_arn}"
     dms_instance_name       = "${module.dms_instance.dms_instance_name}"
     dms_instance_key_arn    = "${module.dms_instance.dms_key_arn}"
-    source_schema           = "tock"
     source_url              = "jdbc://somesource"
     source_username         = "src_user"
     source_password         = "src_pass"
